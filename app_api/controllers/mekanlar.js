9522c30a60b8eadb96e355ceac5858f396b4bb51
kemal özkan <kemal.ozkan.ozkan@gmail.com>
2021-01-16 23:47:48 +0300

    Get fonksiyonları eklendi

--- app_api/controllers/mekanlar.js
+++ app_api/controllers/mekanlar.js
@@ -5,11 +5,52 @@ const cevapOlustur = function(res,status,content){
       .status(status)
       .json(content);
 };
-const mekanlariListele= function(req,res){
-    cevapOlustur(res,200,{"durum":"başarılı"});
+const mekanlariListele= async(req,res) =>{
+    var boylam= parseFloat(req.query.boylam);
+    var enlem = parseFloat(req.query.enlem);
+    var nokta = {
+    	type: "Point",
+    	coordinates: [enlem,boylam]
+    };
+    var geoOptions = {
+    	distanceField: "mesafe",
+    	spherical: true,
+    	key:"koordinatlar"
+    };
+    if (!enlem || !boylam)  {
+    	cevapOlustur(res, 404, {
+    		"mesaj": "enlem ve boylam zorunlu parametreler"
+    	});
+    	return; 
+    }
+    try {
+    	const sonuc= await Mekan.aggregate([
+    	{
+    		$geoNear: {
+    			near:nokta,
+    			...geoOptions
+
+    		}
+    	}
+    	]);
+    	const mekanlar= sonuc.map(mekan=> { return {
+    		_id: mekan._id,
+    		ad: mekan.ad,
+    		adres: mekan.adres,
+    		puan: mekan.puan,
+    		imkanlar: mekan.imkanlar,
+    		mesafe: mekan.mesafe.toFixed()+'m',				
+    	} });
+
+    	cevapOlustur (res, 200, mekanlar);
+    }
+    catch(e){
+    	console.log(e);
+    	cevapOlustur (res, 404, e);
+    }
 }
-const mekanEkle= function(req,res){
-    cevapOlustur(res,200,{"durum":"başarılı"});
+const mekanEkle= function (req, res) {
+	cevapOlustur(res,200,{"durum":"başarılı"});
 }
 const mekanGetir= function(req,res){
     if(req.params&&req.params.mekanid){
@@ -32,7 +73,7 @@ const mekanGetir= function(req,res){
     cevapOlustur(res,404,{"durum":"istekte mekanid yok"});
 }
 const mekanGuncelle= function(req,res){
-    cevapOlustur(res,200,{"durum":"başarılı"});
+    
 }
 const mekanSil= function(req,res){
     cevapOlustur(res,200,{"durum":"başarılı"});
--- app_api/controllers/yorumlar.js
+++ app_api/controllers/yorumlar.js
@@ -10,7 +10,40 @@ const yorumEkle= function(req,res){
     cevapOlustur(res,200,{"durum":"başarılı"});
 }
 const yorumGetir= function(req,res){
-    cevapOlustur(res,200,{"durum":"başarılı"});
+    if(req.params&&req.params.mekanid && req.params.yorumid){
+        Mekan.findById(req.params.mekanid)
+        .select('ad yorumlar')
+         .exec(
+            function(hata,mekan){
+            var cevap,yorum;
+            if(!mekan){
+                cevapOlustur(res,404,{"mesaj":"mekanid bulunamadı"});
+                return;
+            }else if(hata){
+                cevapOlustur(res,404,hata);
+                return;
+            }
+            if(mekan.yorumlar && mekan.yorumlar.length>0){
+                yorum = mekan.yorumlar.id(req.params.yorumid);
+                if(!yorum){
+                    cevapOlustur(res,404,{"mesaj":"yorumid bulunamadı"})
+                }
+                else{
+                    cevap={
+                        mekan: {
+                            ad: mekan.ad,
+                            id: req.params.mekanid
+                        },
+                        yorum : yorum
+                    };
+                    cevapOlustur(res,200,cevap);
+                }
+            }else{
+                cevapOlustur(res,404,{"mesaj":"Hiç yorum yok"});
+            }
+            }
+        );
+    }
 }
 const yorumGuncelle= function(req,res){
     cevapOlustur(res,200,{"durum":"başarılı"});
