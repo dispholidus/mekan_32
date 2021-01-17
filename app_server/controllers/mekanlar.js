84069b1153139620e894c82097daeaac582ffd53
kemal özkan <kemal.ozkan.ozkan@gmail.com>
2021-01-17 18:50:20 +0300

    Link düzeltildi

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,6 +1,6 @@
 var request = require('postman-request');
 var apiSecenekleri = {
-  sunucu : "http://localhost:3000",
+  sunucu : "http://https://kemalozkan1611012093.herokuapp.com",
   apiYolu: '/api/mekanlar/'
 }
 var istekSecenekleri;
